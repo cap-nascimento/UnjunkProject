6045482e276ff5ef799002fdf55c3b748762992b
cap-nascimento <anascimentovieira2@gmail.com>
2022-04-09 14:07:57 -0300

    Changing some stuff: - Icons and Colors; - Home content;

--- App.tsx
+++ App.tsx
@@ -5,9 +5,27 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
+
+// const storeData = async () => {
+// 	try {
+// 		await AsyncStorage.setItem(
+// 			'@MySuperStore:key',
+// 			'I like to save it.'
+// 		);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+		// useEffect(() => {
+		// 	storeData();
+		// }, [])
 
   if (!isLoadingComplete) {
     return null;
--- components/HomeEdit.tsx
+++ components/HomeEdit.tsx
@@ -2,16 +2,35 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from './Themed';
 
+import { Button, Alert } from 'react-native';
+
+const profilePictureModule = require('react-native-profile-picture');
+
 export default function HomeEdit({ path }: { path: string }) {
+	const ProfilePicture = profilePictureModule.default;
 	return (
-		<View>
-			<Text>
-					Conteúdo da home
-			</Text>
+		<View style={styles.container}>
+			<ProfilePicture 
+				isPicture={true}
+				requirePicture={require('../assets/images/zero-two.jpg')}
+				shape='rounded'
+				width={80}
+				height={80}
+			/>
+			<Button
+				title='Ver plano'
+				onPress={() => {Alert.alert('')}}
+			/>
+			<Button
+				title='Compartilhe seu perfil'
+				onPress={() => {Alert.alert('')}}
+			/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-
+	container: {
+		alignItems: 'center',
+	}
 });
